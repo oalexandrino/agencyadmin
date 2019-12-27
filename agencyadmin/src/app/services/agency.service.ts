3d3232d62d8d0437affc89abf9160f8524652a0a
Olavo Alexandrino <oalexandrino@gmail.com>
2019-12-26 22:21:12 -0200

    agency service

---
+++ agencyadmin/src/app/services/agency.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AgencyService {
+
+  constructor(public db: AngularFirestore) {}
+
+  getListing(collectionName: string) {
+    return this.db.collection(collectionName).snapshotChanges();
+  }
+
+  get(collectionName: string, key) {
+    return this.db.collection(collectionName).doc(key).snapshotChanges();
+  }
+
+  delete(collectionName: string, key) {
+    return this.db.collection(collectionName).doc(key).delete();
+  }
+
+  update(collectionName: string, key, value) {
+    value.nameToSearch = value.name.toLowerCase();
+    return this.db.collection(collectionName).doc(key).set(value);
+  }
+
+  insert(collectionName: string, value) {
+    return this.db.collection(collectionName).add({
+      name: value.name,
+      desc: value.desc,
+      price: parseInt(value.price),
+    });
+  }
+
+}
--- agencyadmin/src/app/services/portfolio/portfolio-service.service.ts
+++ agencyadmin/src/app/services/portfolio/portfolio-service.service.ts
@@ -31,6 +31,6 @@ export class PortfolioService {
       desc: value.desc,
       price: parseInt(value.price),
     });
-  }  
+  }
 
 }
