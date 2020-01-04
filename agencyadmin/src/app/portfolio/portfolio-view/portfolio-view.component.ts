9983e3ac880e433f821ca81453529a7bc26eec0c
Olavo Alexandrino <oalexandrino@gmail.com>
2020-01-04 20:11:01 -0200

    implementing feature that shows only available portfolios

--- agencyadmin/src/app/portfolio/portfolio-view/portfolio-view.component.ts
+++ agencyadmin/src/app/portfolio/portfolio-view/portfolio-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, Params } from '@angular/router';
-import { PortfolioService } from './../../services/portfolio/portfolio-service.service';
+import { PortfolioService, PortfolioByDate } from './../../services/portfolio/portfolio-service.service';
 import { AgencyService } from 'src/app/services/agency.service';
 import { MatSlideToggleChange } from '@angular/material';
 
@@ -13,6 +13,7 @@ export class PortfolioViewComponent implements OnInit {
   selectedPrice = 0;
   searchValue = '';
   portfolioItems: Array<any>;
+  portfolioNoAvailableItems: Array<any>;
   portfolioFilteredItemsByPrice: Array<any>;
   portfolioFilteredItemsByName: Array<any>;
   priceValue = 0;
@@ -20,6 +21,7 @@ export class PortfolioViewComponent implements OnInit {
   noPrice = false;
   noDescription = false;
   noEditing = false;
+  noAvailableItems = false;
 
   constructor(
     public firebaseAgencyService: AgencyService,
@@ -58,54 +60,91 @@ export class PortfolioViewComponent implements OnInit {
     }
   }
 
+  toggleHideNoAvailableItems(event: MatSlideToggleChange) {
+    console.log('toggleHideNaAvailableItems');
+    if (!this.noAvailableItems) {
+      this.setOldItems();
+      this.noAvailableItems = true;
+    } else {
+      this.setAllItems();
+      this.noAvailableItems = false;
+    }
+  }
+
+  private setAllItems() {
+    this.firebasePortfolioService.getPortfolioBydate(PortfolioByDate.All)
+      .subscribe(result => {
+        this.portfolioNoAvailableItems = result;
+        this.portfolioItems = this.combineLists(result, this.portfolioFilteredItemsByName);
+        this.portfolioItems = this.combineLists(this.portfolioItems, this.portfolioFilteredItemsByPrice);
+      });
+  }
+
+  private setOldItems() {
+    const currentDate = new Date();
+    this.firebasePortfolioService.getPortfolioBydate(PortfolioByDate.Old, currentDate)
+      .subscribe(result => {
+        this.portfolioNoAvailableItems = result;
+        this.portfolioItems = this.combineLists(result, this.portfolioFilteredItemsByName);
+        this.portfolioItems = this.combineLists(this.portfolioItems, this.portfolioFilteredItemsByPrice);
+      });
+  }
+
   getData() {
     this.firebaseAgencyService.getListing('portfolio')
     .subscribe(result => {
-      this.portfolioItems = result;
-      this.portfolioFilteredItemsByPrice = result;
-      this.portfolioFilteredItemsByName = result;
+      this.setPortfolioArrays(result);
     });
   }
+
   viewPortfolioDetails(item) {
     this.router.navigate(['/portfolio-details/' + item.payload.doc.id]);
   }
 
   searchByName() {
-    const value = this.searchValue.toLowerCase();
-    this.firebaseAgencyService.searchByValue('portfolio', 'name', this.searchValue)
-    .subscribe(result => {
-      this.portfolioFilteredItemsByName = result;
-      this.portfolioItems = this.combineLists(result, this.portfolioFilteredItemsByPrice);
-    });
+      const value = this.searchValue.toLowerCase();
+      this.firebaseAgencyService.searchByValue('portfolio', 'name', this.searchValue)
+      .subscribe(result => {
+        this.portfolioFilteredItemsByName = result;
+        this.portfolioItems = this.combineLists(result, this.portfolioFilteredItemsByPrice);
+        this.portfolioItems = this.combineLists(this.portfolioItems, this.portfolioNoAvailableItems);
+      });
   }
 
-  rangeOfPricesChange(event) {
+  searchByRangeOfPrices(event) {
 
-    this.selectedPrice = event.value;
-    this.firebasePortfolioService.getPorfolioByPrice(event.value)
-    .subscribe(result => {
-      this.portfolioFilteredItemsByPrice = result;
-      this.portfolioItems = this.combineLists(result, this.portfolioFilteredItemsByName);
+      this.selectedPrice = event.value;
+      this.firebasePortfolioService.getPorfolioByPrice(event.value)
+      .subscribe(result => {
+        this.portfolioFilteredItemsByPrice = result;
+        this.portfolioItems = this.combineLists(result, this.portfolioFilteredItemsByName);
+        this.portfolioItems = this.combineLists(this.portfolioItems, this.portfolioNoAvailableItems);
 
-      if (result.length === 0) {
-        this.noElementsMessage  = true;
-      } else {
-        this.noElementsMessage  = false;
-      }
-    });
-  }
-
-  combineLists(a, b) {
-    const result = [];
-
-    a.filter(x => {
-      return b.filter(x2 => {
-        if (x2.payload.doc.id === x.payload.doc.id) {
-          result.push(x2);
+        if (result.length === 0) {
+          this.noElementsMessage  = true;
+        } else {
+          this.noElementsMessage  = false;
         }
       });
-    });
-    return result;
   }
 
+  private combineLists(a, b) {
+  const result = [];
+
+  a.filter(x => {
+    return b.filter(x2 => {
+      if (x2.payload.doc.id === x.payload.doc.id) {
+        result.push(x2);
+      }
+    });
+  });
+  return result;
+  }
+
+  private setPortfolioArrays(result) {
+    this.portfolioItems = result;
+    this.portfolioNoAvailableItems = result;
+    this.portfolioFilteredItemsByPrice = result;
+    this.portfolioFilteredItemsByName = result;
+  }
 }
--- agencyadmin/src/app/services/portfolio/portfolio-service.service.ts
+++ agencyadmin/src/app/services/portfolio/portfolio-service.service.ts
@@ -2,12 +2,48 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
+export enum PortfolioByDate {
+  New,
+  Old,
+  All,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PortfolioService {
 
   constructor(public db: AngularFirestore) {}
+
+  getPortfolioBydate(type: PortfolioByDate, date?: Date ) {
+    let portfolioItems;
+
+    switch (type) {
+
+      case PortfolioByDate.All:
+        portfolioItems = this.db.collection('portfolio', ref => ref
+          .orderBy('availableDate'))
+          .snapshotChanges();
+        break;
+
+      case PortfolioByDate.New:
+        portfolioItems = this.db.collection('portfolio', ref => ref
+          .where('availableDate', '>', date)
+          .orderBy('availableDate'))
+          .snapshotChanges();
+        break;
+
+      case PortfolioByDate.Old:
+        portfolioItems = this.db.collection('portfolio', ref => ref
+          .where('availableDate', '<', date)
+          .orderBy('availableDate'))
+          .snapshotChanges();
+        break;
+    }
+
+    return portfolioItems;
+
+  }
 
   getPorfolioByPrice(value) {
     return this.db.collection('portfolio', ref => ref.orderBy('price').startAt(value)).snapshotChanges();
