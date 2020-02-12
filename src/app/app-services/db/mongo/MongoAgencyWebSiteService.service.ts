7f44bf5167025b47805a46f74498d49ba53f18d4
Olavo Alexandrino <oalexandrino@gmail.com>
2020-02-12 02:32:15 -0200

    testing 2nd key repo

--- src/app/app-services/db/mongo/MongoAgencyWebSiteService.service.ts
+++ src/app/app-services/db/mongo/MongoAgencyWebSiteService.service.ts
@@ -5,9 +5,7 @@ import { Observable, of, throwError } from 'rxjs';
 import { catchError, tap, map } from 'rxjs/operators';
 import { Service } from 'src/model/service';
 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
+const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
 
 @Injectable({
   providedIn: 'root'
@@ -16,18 +14,11 @@ export class MongoAgencyWebSiteService {
 
   apiURL = 'http://localhost:3000/api/';
 
-  constructor(
-    public router: Router,
-    public http: HttpClient
-  ) {
-
-  }
+  constructor(public router: Router, public http: HttpClient ) {  }
 
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-
       console.error(error);
-
       return of(result as T);
     };
   }
@@ -41,7 +32,6 @@ export class MongoAgencyWebSiteService {
         tap(serviceItems => console.log(`reading listing of ${endpoint} endpoint`)),
         catchError(this.handleError('getListing', []))
       );
-
   }
 
   update(endpoint: string, updateOptions: any): Observable<any> {
@@ -52,7 +42,6 @@ export class MongoAgencyWebSiteService {
       tap(_ => console.log(`updating item id=${updateOptions.id} for the ${endpoint} endpoint`)),
       catchError(this.handleError<any>('update'))
     );
-
   }
 
   getCollectionItem(endpoint: string, key: string): Observable<any> {
@@ -73,19 +62,5 @@ export class MongoAgencyWebSiteService {
       tap(_ => console.log(`document item id=${deleteOptions.id} for the ${endpoint} endpoint has been deleted`)),
       catchError(this.handleError<any>('delete'))
     );
-
-
   }
-
-
-  /*
-    insert(endpoint: string, value) {
-
-    }
-
-    delete(endpoint: string, key) {
-
-    }
-  */
-
 }
